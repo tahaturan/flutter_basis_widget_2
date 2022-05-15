dbac1541ad8f1d7756449484136f6f8a939d4942
Taha Turan <tahaaturann16@gmail.com>
2022-05-15 07:04:18 +0300

    Toggle Button Kullanimi

--- lib/views/my_home_page.dart
+++ lib/views/my_home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_basis_widget_2/views/toggle_buttons_kullanimi.dart';
+import 'package:flutter_basis_widget_2/widgets/sayfa_gecis_button.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
@@ -14,8 +16,15 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: const Text("Ana Sayfa"),
       ),
-      body: Column(
-        children: [],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SayfaGecisButton(
+                butonYazi: "Toggle Button",
+                gecisSayfasi: const ToggleButtonsKullanimi()),
+          ],
+        ),
       ),
     );
   }
--- lib/views/toggle_buttons_kullanimi.dart
+++ lib/views/toggle_buttons_kullanimi.dart
@@ -8,13 +8,43 @@ class ToggleButtonsKullanimi extends StatefulWidget {
 }
 
 class _ToggleButtonsKullanimiState extends State<ToggleButtonsKullanimi> {
+  var toggleDurumlar = [false, true, false];
+  int secilenToggleIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Toggle Buttons Kullanimi"),
       ),
-      body: Container(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ToggleButtons(
+              color: Colors.grey, //* secilmemis haldeki rengini belirler
+              selectedColor: Colors.amber, //*secilidi zaman aldigi renk
+              fillColor: Colors.cyan, //* secildigi zaman olan arka plan rengi
+              borderColor: Colors.red, //* secilmemis haldeki cercevere rengi
+              selectedBorderColor:
+                  Colors.green, //*secilmis haldeki cerceve rengi
+              borderWidth: 3, //* cerceve kalinligi
+              isSelected: toggleDurumlar,
+              children: const [
+                Icon(Icons.looks_one),
+                Icon(Icons.looks_two),
+                Icon(Icons.looks_3),
+              ],
+              onPressed: (int secilenIndex) {
+                secilenToggleIndex = secilenIndex;
+                debugPrint("${secilenIndex + 1}.Toggle Secildi");
+                setState(() {
+                  toggleDurumlar[secilenIndex] = !toggleDurumlar[secilenIndex];
+                });
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
